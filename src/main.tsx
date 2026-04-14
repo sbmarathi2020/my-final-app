59db598ea375e57aad0c4ac671bebd4755c4e5d2
sbmarathi2020 <sbmarathi2020@gmail.com>
2026-04-15 01:35:40 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,22 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
 
-
-import { registerSW } from 'virtual:pwa-register';
-
-if ('serviceWorker' in navigator) {
-    registerSW({
-        onNeedRefresh() {
-            if (confirm("नवीन अपडेट उपलब्ध आहे. ॲप रिफ्रेश करायचे का?")) {
-                window.location.reload();
-            }
-        },
-        onOfflineReady() {
-            console.log("ॲप आता ऑफलाइन वापरण्यासाठी तयार आहे!");
-        },
-    });
-}
-
+// PWA चा भाग पूर्णपणे काढून टाकला आहे कारण तो बिल्ड एरर देत होता
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <BrowserRouter>
